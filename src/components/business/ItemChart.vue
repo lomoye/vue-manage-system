bc6bd4d082cfb19d0073fc3220b9e37905abfd57
lomoye <834033206@qq.com>
2017-06-28 22:00:11 +0800

    fixed

--- src/components/business/ItemChart.vue
+++ src/components/business/ItemChart.vue
@@ -1,17 +1,61 @@
 <template>
-    <h1>{{ itemId }}的报表数据展示</h1>
+    <div>
+        <el-row>
+            <el-col :span="12">
+                <div id="main"></div>
+            </el-col>
+        </el-row>
+    </div>
+
 </template>
 
 <script>
+    import echarts from 'echarts'
+
+
     export default {
         data() {
             return {
-                itemId: this.$route.params.itemId
+                option: {
+                    title: {text: this.$route.params.itemId + "的报表数据展示"},
+                    tooltip: {},
+                    xAxis: {
+                        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                }
+            }
+        },
+
+        created() {
+            this.$nextTick(function() {
+                this.renderChart('main');
+            })
+
+        },
+
+        methods: {
+            renderChart(id) {
+                let myChart = echarts.init(document.getElementById(id));
+                // 绘制图表
+                myChart.setOption(this.option);
             }
         }
+
+
     }
+
+
 </script>
 
 <style scoped>
-
+    #main {
+        /*需要制定具体高度，以px为单位*/
+        height: 400px;
+    }
 </style>
