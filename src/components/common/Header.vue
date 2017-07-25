4909744d9e473b436e3ad1adf7110a1558d3248b
lomoye <834033206@qq.com>
2017-07-25 17:47:06 +0800

    fixed

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header">
-        <div class="logo">数据记录系统</div>
+        <div class="logo">沉迷学习</div>
         <div class="user-info">
             <el-dropdown trigger="click" @command="handleCommand">
                 <span class="el-dropdown-link">
--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="login-wrap">
-        <div class="ms-title">数据记录系统</div>
+        <div class="ms-title">沉迷学习</div>
         <div class="ms-login">
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
                 <el-form-item prop="mobile">
